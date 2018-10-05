47e14ee28f715ac0418fe2e3fed80a3d82c5a002
manuel14 <manuelzubieta14@gmail.com>
2018-10-05 16:46:57 -0300

    Changing error mid to expect 400 instead of 401 status

--- src/middleware.js
+++ src/middleware.js
@@ -26,7 +26,7 @@ const authMiddleware = ({ dispatch }) => next => action => {
 const errorMiddleware = ({ dispatch }) => next => action => {
   console.log(action);
   console.log(action.error);
-  if (action.error && action.error.status !== 401) {
+  if (action.error && action.error.status !== 400) {
     const error = get(action.error.body, 'error');
     const { title, message } = error ? error : DEFAULT_ERROR;
     const notification = {
